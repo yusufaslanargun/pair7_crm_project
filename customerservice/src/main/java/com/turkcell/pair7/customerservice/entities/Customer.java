a7a74fd323301a2557d0e7d1b8fb57eb04cbc501
yusufaslanargun <yusufaslanargun@gmail.com>
2024-03-16 17:46:42 +0300

    entities last update

--- customerservice/src/main/java/com/turkcell/pair7/customerservice/entities/Customer.java
+++ customerservice/src/main/java/com/turkcell/pair7/customerservice/entities/Customer.java
@@ -1,28 +1,54 @@
 package com.turkcell.pair7.customerservice.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Table(name = "customers")
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Customer {
+    @Column(name = "customer_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int customerId;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "middle_name")
     private String middleName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "birth_date")
     private Date birthDate;
+
+    @Column(name = "gender")
     private int gender;
+
+    @Column(name = "father_name")
     private String fatherName;
+
+    @Column(name = "mother_name")
     private String motherName;
+
+    @Column(name = "nationality_id")
     private int nationalityId;
+
+    @Column(name = "gsm_number")
     private String gsmNumber;
-    private int accountNumber;
+
+    @Column(name = "order_number")
     private int orderNumber;
 
+    @Column(name = "account_number")
+    private int accountNumber;
+    
     public int getCustomerId() {
         return customerId;
     }
